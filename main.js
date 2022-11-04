160e694851d905be8519e013b925136a8bf8b0de
Giancarlo Pagliarini Alves <giancarlopalvess@outlook.com>
2022-11-04 11:39:36 -0300

    add: novos cards e rodapé

--- main.js
+++ main.js
@@ -22,8 +22,10 @@ function creatCard (date, day, games) {
 }
 
 document.querySelector('#cards').innerHTML = 
-    creatCard("24/11", "quinta", creatGame('brazil', '16:00', 'serbia')) 
-    + creatCard("28/11", "segunda", creatGame('brazil', '13:00', 'switzerland') 
-    + creatGame('portugal', '16:00', 'uruguay')) 
-    + creatCard("02/12", "sexta", creatGame('brazil', '16:00', 'cameroon'))
+    creatCard("24/11", "quinta", creatGame('switzerland', '07:00', 'cameroon') + creatGame('uruguay', '10:00', 'southkorea') + creatGame('portugal', '13:00', 'ghana') + creatGame('brazil', '16:00', 'serbia')) 
+    + creatCard("28/11", "segunda", creatGame('cameroon', '07:00', 'serbia') 
+    + creatGame('southkorea', '10:00', 'ghana') + creatGame('brazil', '13:00', 'switzerland') + creatGame('portugal', '16:00', 'uruguay')) 
+    + creatCard("01/12", "quinta", creatGame('croatia', '12:00', 'belgium') + creatGame('canada', '12:00', 'morocco') + creatGame('japan', '16:00', 'spain') + creatGame('costarica', '16:00', 'germany'))
+    + creatCard("02/12", "sexta", creatGame('southkorea', '12:00', 'portugal') + creatGame('ghana', '12:00', 'uruguay') + creatGame('serbia', '16:00', 'switzerland') + creatGame('brazil', '16:00', 'cameroon'))
+    creatCard('Link para todos os')
 
